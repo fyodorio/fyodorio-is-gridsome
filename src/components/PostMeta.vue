01840c2711596dfb78e8c82aff0759764ddd9e75
fyodorio <alinetofyodor@gmail.com>
2023-01-05 15:12:42 +0500

    chore: add posted on

--- src/components/PostMeta.vue
+++ src/components/PostMeta.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="post-meta">
-      Posted {{ post.date }}.
+      Posted on {{ post.date }}.
       <template v-if="post.timeToRead">
         <strong>{{ post.timeToRead }} min read.</strong>
       </template>
