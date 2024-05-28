1419f63df5f85eed4c8c9fc72edd39632a831b7a
fyodorio <alinetofyodor@gmail.com>
2024-05-28 21:08:42 +0500

    chore: test font preloading

--- src/main.js
+++ src/main.js
@@ -6,7 +6,18 @@ import DefaultLayout from '~/layouts/Default.vue'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
-  
+  // Preload Google Fonts
+  head.link.push({
+    rel: 'preload',
+    as: 'style',
+    href: 'https://fonts.googleapis.com/css2?family=Walter+Turncoat&display=optional'
+  })
+  head.link.push({
+    rel: 'preload',
+    as: 'style',
+    href: 'https://fonts.googleapis.com/css?family=Poppins:400,600&display=optional'
+  })
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
