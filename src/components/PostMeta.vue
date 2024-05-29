138585fc091ef5868de58042ac1f57a6b30ab05b
fyodorio <alinetofyodor@gmail.com>
2024-05-29 08:34:46 +0500

    feat: lore-specific post meta texts

--- src/components/PostMeta.vue
+++ src/components/PostMeta.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="post-meta">
-      <span>Posted on {{ post.date }}. </span>
+      <span>Crafted on {{ post.date }}. </span>
       <template v-if="post.updatedOn">
-        <span>Updated on {{ post.updatedOn }}. </span>
+        <span>Repaired on {{ post.updatedOn }}. </span>
       </template>
       <template v-if="post.timeToRead">
         <strong>{{ post.timeToRead }} min read.</strong>
