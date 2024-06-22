eb282e047644c8c49a0a632c7f29d9c91ae3468c
fyodorio <alinetofyodor@gmail.com>
2024-06-22 19:12:33 +0500

    fix: poppins loading; new emoji favicon

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ export default function (Vue, { router, head, isClient }) {
   head.link.push({
     rel: 'preload',
     as: 'style',
-    href: 'https://fonts.googleapis.com/css2?family=Poppins:400,600&display=swap'
+    href: 'https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap'
   })
 
   // Load Google Fonts
@@ -34,7 +34,13 @@ export default function (Vue, { router, head, isClient }) {
   })
   head.link.push({
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Poppins:400,600&display=swap'
+    href: 'https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap'
+  })
+
+  // Set emoji favicon
+  head.link.push({
+    rel: 'icon',
+    href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🛠️</text></svg>'
   })
 
   // Set default layout as a global component
